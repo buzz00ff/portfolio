86863962de34861faad1e9608b78497f99cbc2f1
buzz00ff <137896111+buzz00ff@users.noreply.github.com>
2024-07-07 21:41:54 +0200

    Add files via upload

---
+++ TicTacToe/TicTacToe/Board.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Board
+    {
+        private char[,] board;
+
+        public Board()
+        {
+            board = new char[3, 3];
+            InitializeBoard();
+        }
+
+        private void InitializeBoard()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    board[i, j] = ' ';
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(" " + board[i, 0] + " | " + board[i, 1] + " | " + board[i, 2]);
+                if (i < 2)
+                    Console.WriteLine("---|---|---");
+            }
+            Console.WriteLine();
+        }
+
+        public bool IsValidMove(int row, int col)
+        {
+            return row >= 0 && row < 3 && col >= 0 && col < 3 && board[row, col] == ' ';
+        }
+
+        public void PlaceMove(int row, int col, char symbol)
+        {
+            board[row, col] = symbol;
+        }
+
+        public bool IsFull()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == ' ')
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public char[,] GetBoardState()
+        {
+            return board;
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/Game.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Game
+    {
+        private Board board;
+        private bool playerTurn; // true - Player X, false - Computer O
+
+        public Game()
+        {
+            board = new Board();
+            playerTurn = true; // Rozpoczęcie gry od gracza X
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Welcome to Tic-Tac-Toe!");
+
+            while (true)
+            {
+                board.Draw();
+
+                int row, col;
+                if (playerTurn)
+                {
+                    Console.WriteLine("Player's turn (X)");
+                    (row, col) = GetPlayerMove();
+                }
+                else
+                {
+                    Console.WriteLine("Computer's turn (O)");
+                    (row, col) = GetComputerMove();
+                }
+
+                if (board.IsValidMove(row, col))
+                {
+                    char symbol = playerTurn ? 'X' : 'O';
+                    board.PlaceMove(row, col, symbol);
+
+                    if (CheckWin(symbol))
+                    {
+                        board.Draw();
+                        Console.WriteLine((playerTurn ? "Player" : "Computer") + " wins!");
+                        break;
+                    }
+
+                    if (board.IsFull())
+                    {
+                        board.Draw();
+                        Console.WriteLine("It's a draw!");
+                        break;
+                    }
+
+                    playerTurn = !playerTurn; // Przełącz na następnego gracza
+                }
+                else
+                {
+                    Console.WriteLine("Invalid move. Try again.");
+                }
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private (int, int) GetPlayerMove()
+        {
+            Console.Write("Enter row (1-3): ");
+            int row = int.Parse(Console.ReadLine()) - 1;
+            Console.Write("Enter column (1-3): ");
+            int col = int.Parse(Console.ReadLine()) - 1;
+            return (row, col);
+        }
+
+        private (int, int) GetComputerMove()
+        {
+            Random random = new Random();
+            int row, col;
+            do
+            {
+                row = random.Next(3);
+                col = random.Next(3);
+            } while (!board.IsValidMove(row, col));
+            return (row, col);
+        }
+
+        private bool CheckWin(char symbol)
+        {
+            char[,] state = board.GetBoardState();
+            // Sprawdzenie poziomo, pionowo i na ukos
+            for (int i = 0; i < 3; i++)
+            {
+                if (state[i, 0] == symbol && state[i, 1] == symbol && state[i, 2] == symbol)
+                    return true;
+                if (state[0, i] == symbol && state[1, i] == symbol && state[2, i] == symbol)
+                    return true;
+            }
+            if (state[0, 0] == symbol && state[1, 1] == symbol && state[2, 2] == symbol)
+                return true;
+            if (state[0, 2] == symbol && state[1, 1] == symbol && state[2, 0] == symbol)
+                return true;
+            return false;
+        }
+    }
+}
